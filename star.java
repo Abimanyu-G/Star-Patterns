5ec29613277a568dd2766f4d1028b2ca49955678
Abimanyu-G <abimanyuganesan3@gmail.com>
2025-10-03 10:49:34 +0530

    star file dooii

---
+++ star.java
@@ -0,0 +1,121 @@
+public class star{
+    public static void main(String args[]){
+
+     
+     /* ***
+        ***
+        ***   <-indha pattern ku */
+
+        for(int i = 0; i<5; i++){
+        
+            for(int j = 0; j<5; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+System.out.println();
+
+        /*  *
+            * *
+            * * *
+            * * * *      <-- indha pattern ku */
+
+        for(int i =0; i<5; i++){
+            for(int j = i; j>=0; j--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+System.out.println();
+
+
+       /*
+        1
+        1 2
+        1 2 3
+        1 2 3 4
+        1 2 3 4 5       <-- indha pattern ku  */
+
+        for(int i = 1; i<=5; i++){
+            for(int j = 1; j<=i; j++){
+                System.out.print( j +  " ");
+            }
+            System.out.println();
+        }
+     System.out.println();
+
+
+
+     /*
+        * * * *
+        * * *
+        * *
+        *               <--indha pattern ku */
+
+      for(int i =  0; i<5; i++){
+        for(int j = i; j<5 ; j++){
+            System.out.print("* ");
+        }
+        System.out.println();
+      }
+System.out.println();
+
+ 
+     /*
+        1 2 3 4 5
+        1 2 3 4
+        1 2 3 
+        1 2
+        1        <-- indha pattern ku  */
+
+        for(int i=5; i>0; i--){
+         for(int j = 1 ; j<=i; j++){
+            System.out.print(j + " ");
+         }
+         System.out.println();
+        }
+
+
+    /*      *
+          * * *  
+        * * * * *
+      * * * * * * *    <-- indha pattern ku  */
+
+     for(int i =0; i<5;i++){
+        for(int j = 0; j < 5 -i-1; j++){
+            System.out.print(" ");
+        }
+        for(int j = 0; j < i*2+1; j++){
+            System.out.print("*");
+        }
+        for(int j = 0; j < 5 -i-1; j++){
+            System.out.print(" ");
+        }
+        System.out.println();
+     }
+     System.out.println();
+
+
+
+
+     /*   * * * * *
+           * * * *
+            * * *
+             * *
+              *            <--for this  */
+
+             for(int i = 0; i<5; i++){
+                for(int j=0; j<i; j++){
+                    System.out.print(" ");
+                }
+                for(int j = 0; j<(2*5 - (2*i +1)); j++){
+                    System.out.print("*");
+                }
+                for(int j=0; j<i; j++){
+                    System.out.print(" ");
+                }
+                System.out.println();
+             }
+    }
+}
